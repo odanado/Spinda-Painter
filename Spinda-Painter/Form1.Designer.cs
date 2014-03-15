852eed608c8b025bf50b33ff6bade7ad7344bce6
odan <odan3240@gmail.com>
2014-03-15 19:30:53 +0900

    First commit

---
+++ Spinda-Painter/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Spinda_Painter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.spot4 = new System.Windows.Forms.PictureBox();
+            this.spot3 = new System.Windows.Forms.PictureBox();
+            this.spot2 = new System.Windows.Forms.PictureBox();
+            this.spot1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBoxResult = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.spot4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 12);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "88888888";
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // spot4
+            // 
+            this.spot4.BackColor = System.Drawing.Color.Transparent;
+            this.spot4.BackgroundImage = global::Spinda_Painter.Properties.Resources._327_4_8;
+            this.spot4.Location = new System.Drawing.Point(128, 154);
+            this.spot4.Name = "spot4";
+            this.spot4.Size = new System.Drawing.Size(64, 72);
+            this.spot4.TabIndex = 4;
+            this.spot4.TabStop = false;
+            this.spot4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.spot4_MouseDown);
+            this.spot4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.spot4_MouseMove);
+            // 
+            // spot3
+            // 
+            this.spot3.BackColor = System.Drawing.Color.Transparent;
+            this.spot3.BackgroundImage = global::Spinda_Painter.Properties.Resources._327_3_8;
+            this.spot3.Location = new System.Drawing.Point(26, 148);
+            this.spot3.Name = "spot3";
+            this.spot3.Size = new System.Drawing.Size(56, 72);
+            this.spot3.TabIndex = 3;
+            this.spot3.TabStop = false;
+            this.spot3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.spot3_MouseDown);
+            this.spot3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.spot3_MouseMove);
+            // 
+            // spot2
+            // 
+            this.spot2.BackColor = System.Drawing.Color.Transparent;
+            this.spot2.BackgroundImage = global::Spinda_Painter.Properties.Resources._327_2_8;
+            this.spot2.Location = new System.Drawing.Point(128, 46);
+            this.spot2.Name = "spot2";
+            this.spot2.Size = new System.Drawing.Size(104, 104);
+            this.spot2.TabIndex = 2;
+            this.spot2.TabStop = false;
+            this.spot2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.spot2_MouseDown);
+            this.spot2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.spot2_MouseMove);
+            // 
+            // spot1
+            // 
+            this.spot1.BackColor = System.Drawing.Color.Transparent;
+            this.spot1.BackgroundImage = global::Spinda_Painter.Properties.Resources._327_1_8;
+            this.spot1.Location = new System.Drawing.Point(26, 46);
+            this.spot1.Name = "spot1";
+            this.spot1.Size = new System.Drawing.Size(96, 96);
+            this.spot1.TabIndex = 1;
+            this.spot1.TabStop = false;
+            this.spot1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.spot1_MouseDown);
+            this.spot1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.spot1_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Spinda_Painter.Properties.Resources._327_0_8;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(421, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 16);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "グリッド線を表示";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // pictureBoxResult
+            // 
+            this.pictureBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxResult.Location = new System.Drawing.Point(458, 37);
+            this.pictureBoxResult.Name = "pictureBoxResult";
+            this.pictureBoxResult.Size = new System.Drawing.Size(64, 64);
+            this.pictureBoxResult.TabIndex = 7;
+            this.pictureBoxResult.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(426, 552);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Produced by odan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(535, 567);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxResult);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.spot4);
+            this.Controls.Add(this.spot3);
+            this.Controls.Add(this.spot2);
+            this.Controls.Add(this.spot1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "3gen-Spinda-Painter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.spot4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.spot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxResult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox spot1;
+        private System.Windows.Forms.PictureBox spot2;
+        private System.Windows.Forms.PictureBox spot3;
+        private System.Windows.Forms.PictureBox spot4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBoxResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Spinda-Painter/Form1.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spinda_Painter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private UInt32 PID;
+
+        private int magnification = 8;
+        private Color spindaColor = Color.FromArgb(0xAF, 0xDE, 0x6B, 0x39);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            PID = Convert.ToUInt32(textBox1.Text,16);
+
+            for (int index = 0; index < 4; index++)
+            {
+
+                lower[2 * index] = coords[2 * index] + origin[0];
+                lower[2 * index + 1] = coords[2 * index + 1] + origin[1];
+
+                upper[2 * index] = 0xf + coords[2 * index] + origin[0];
+                upper[2 * index + 1] = 0xf + coords[2 * index + 1] + origin[1];
+
+                lower[2 * index] *= magnification;
+                lower[2 * index + 1] *= magnification;
+
+                upper[2 * index] *= magnification;
+                upper[2 * index + 1] *= magnification;
+            }
+
+            pictureBox1.Controls.Add(spot1);
+            pictureBox1.Controls.Add(spot2);
+            pictureBox1.Controls.Add(spot3);
+            pictureBox1.Controls.Add(spot4);
+
+            setSpot(spot1, 0);
+            setSpot(spot2, 1);
+            setSpot(spot3, 2);
+            setSpot(spot4, 3);
+
+            drawResult();
+            setLabel();
+        }
+
+        void drawResult()
+        {
+            Bitmap spinda = Properties.Resources._327_0;
+            Bitmap spot = new Bitmap(64, 64);
+
+            //spinda.SetPixel(0, 0, spindaColor);
+            Color color = spinda.GetPixel(0, 0);
+
+            for (int i = 0; i < 4; i++)
+            {
+                int left = (int)(PID >> (i * 8)) & 0xF;
+                int top = (int)(PID >> (i * 8 + 4)) & 0xF;
+
+                int x = left + coords[2 * i] + origin[0];
+                int y = top + coords[2 * i + 1] + origin[1];
+
+                for (int j = 0; j < spots[i].Length; j++)
+                {
+                    for (int k = 0; k < spots[i][j].Length; k++)
+                    {
+                        int nx = x + j;
+                        int ny = y + k;
+
+                        if (spots[i][j][k] == '*' && spinda.GetPixel(nx, ny) != color)
+                        {
+                            spot.SetPixel(nx, ny, spindaColor);
+                        }
+
+                    }
+                }
+
+            }
+
+
+            pictureBoxResult.BackgroundImage = spinda;
+            pictureBoxResult.Image = spot;
+
+
+        }
+
+        void setLabel()
+        {
+            String nature = natures[PID % 25];
+            String sex = (PID & 0xff) < 127 ? "♀" : "♂";
+            String ability = (PID & 1) == 0 ? "マイペース":"ちどりあし";
+
+            label1.Text = String.Format("({0} / {1} / {2})", nature, sex, ability);
+
+        }
+
+        void drawGrid(PictureBox picBox)
+        {
+            Bitmap canvas = new Bitmap(picBox.Width, picBox.Height);
+            Graphics g = Graphics.FromImage(canvas);
+
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            for (int i = 0; i < 64; i++)
+            {
+                int y = i * magnification;
+
+                Point point1 = new Point(0, y);
+                Point point2 = new Point(pictureBox1.Height, y);
+
+                g.DrawLine(blackPen, point1, point2);
+            }
+            
+            for (int i = 0; i < 64; i++)
+            {
+
+                int x = i * magnification;
+                Point point1 = new Point(x, 0);
+                Point point2 = new Point(x, pictureBox1.Width);
+
+                g.DrawLine(blackPen, point1, point2);
+            }
+
+            g.Dispose();
+            picBox.Image = canvas;
+        }
+
+        private int[] coords = { 0, 0, 24, 1, 6, 18, 18, 19 };
+        private int[] origin = { 8, 6 };
+        private int[] lower = new int[8];
+        private int[] upper = new int[8];
+
+        private void setSpot(PictureBox picBox, int index)
+        {
+            int left  = (int) (PID >> (index * 8)) & 0xF;
+            int top   = (int) (PID >> (index * 8 + 4)) & 0xF;
+
+            left = left + coords[2 * index] + origin[0];
+            top  = top  + coords[2 * index + 1] + origin[1];
+
+            picBox.Left = left * magnification;
+            picBox.Top = top * magnification;
+
+
+        }
+
+        private void updatePID(int left, int top, int index)
+        {
+            uint _left = (uint)(left / magnification - coords[2 * index] - origin[0]);
+            uint _top = (uint)(top / magnification - coords[2 * index + 1] - origin[1]);
+
+            uint mask = 0xffffffff;
+            uint xor = (uint) 0xff << (index * 8);
+
+            mask ^= xor;
+
+            PID &= mask;
+            PID |= (_left << (index * 8));
+            PID |= (_top << (index * 8 + 4));
+
+            textBox1.Text = String.Format("{0:X08}", PID);
+        }
+
+        private bool check(int left, int top, int index)
+        {
+            return (lower[2*index] <= left && left <= upper[2*index] &&
+                     lower[2*index+1] <= top && top <= upper[2*index+1]
+                     );
+        }
+
+        #region spots
+        String[][] spots =  {
+            new String[]
+            {"    ***     ",
+             "  *******   ",
+             " *********  ",
+             " ********** ",
+             "*********** ",
+             "************",
+             "************",
+             "************",
+             " ********** ",
+             " ********** ",
+             "  ********  ",
+             "     ****   "},
+             new String[]
+            {"     ****    ",
+             "   *******   ",
+             "  *********  ",
+             " *********** ",
+             " *********** ",
+             "*************",
+             "*************",
+             "*************",
+             " *********** ",
+             " *********** ",
+             "  *********  ",
+             "   ********  ",
+             "     ***     "},
+             new String[]
+            {"  ***  ",
+             " ***** ",
+             "*******",
+             "*******",
+             "*******",
+             "*******",
+             "*******",
+             " ***** ",
+             "  ***  "},
+             new String[]
+            {"  ****  ",
+             " ****** ",
+             "********",
+             "********",
+             "********",
+             "********",
+             "********",
+             " ****** ",
+             "  ****  "}};
+
+        #endregion
+
+        String[] natures = {
+                               "がんばりや","さみしがり","ゆうかん","いじっぱり",
+                               "やんちゃ","ずぶとい","すなお","のんき","わんぱく",
+                               "のうてんき","おくびょう","せっかち","まじめ","ようき",
+                               "むじゃき","ひかえめ","おっとり","れいせい","てれや",
+                               "うっかりや","おだやか","おとなしい",
+                               "なまいき","しんちょう","きまぐれ",
+                           };
+
+        private Point spot1MousePoint;
+        private Point spot2MousePoint;
+        private Point spot3MousePoint;
+        private Point spot4MousePoint;
+
+        private void spot1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                spot1MousePoint = new Point(e.X, e.Y);
+            }
+
+        }
+
+        private void spot1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                int left = spot1.Left + e.X - spot1MousePoint.X;
+                int top = spot1.Top + e.Y - spot1MousePoint.Y;
+
+                if (check(left, top, 0))
+                {
+                    spot1.Left = left - left % magnification;
+                    spot1.Top = top - top % magnification;
+
+                    updatePID(spot1.Left, spot1.Top, 0);
+                    drawResult();
+                    setLabel();
+
+                }
+
+            }
+        }
+
+        private void spot2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                spot2MousePoint = new Point(e.X, e.Y);
+            }
+        }
+
+        private void spot2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                int left = spot2.Left + e.X - spot2MousePoint.X;
+                int top = spot2.Top + e.Y - spot2MousePoint.Y;
+                if (check(left, top, 1))
+                {
+                    spot2.Left = left - left % magnification;
+                    spot2.Top = top - top % magnification;
+
+                    updatePID(spot2.Left, spot2.Top, 1);
+                    drawResult();
+                    setLabel();
+
+                }
+
+            }
+        }
+
+        private void spot3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                spot3MousePoint = new Point(e.X, e.Y);
+            }
+
+        }
+
+        private void spot3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                int left = spot3.Left + e.X - spot3MousePoint.X;
+                int top = spot3.Top + e.Y - spot3MousePoint.Y;
+
+                if (check(left, top, 2))
+                {
+                    spot3.Left = left - left % magnification;
+                    spot3.Top = top - top % magnification;
+
+                    updatePID(spot3.Left, spot3.Top, 2);
+                    drawResult();
+                    setLabel();
+
+                }
+            }
+        }
+
+        private void spot4_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                spot4MousePoint = new Point(e.X, e.Y);
+            }
+
+        }
+
+        private void spot4_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                int left = spot4.Left + e.X - spot4MousePoint.X;
+                int top = spot4.Top + e.Y - spot4MousePoint.Y;
+
+                if (check(left, top, 3))
+                {
+                    spot4.Left = left - left % magnification;
+                    spot4.Top = top - top % magnification;
+
+                    updatePID(spot4.Left, spot4.Top, 3);
+                    drawResult();
+                    setLabel();
+
+                }
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                drawGrid(pictureBox1);
+
+            }
+            else
+            {
+                pictureBox1.Image = null;
+
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (1 <= textBox1.Text.Length && textBox1.Text.Length <= 8)
+            {
+                PID = Convert.ToUInt32(textBox1.Text, 16);
+                setSpot(spot1, 0);
+                setSpot(spot2, 1);
+                setSpot(spot3, 2);
+                setSpot(spot4, 3);
+                drawResult();
+
+                setLabel();
+
+            }
+        }
+    
+
+    }
+}
